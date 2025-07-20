661f3332b0fe643297cb5adf4421ee5cb5b08d6c
Amirmohammad Rammezani <57634297+AmRam841@users.noreply.github.com>
2025-07-20 23:48:25 +0330

    Add files via upload

---
+++ eucladian distance formula.py
@@ -0,0 +1,16 @@
+import math as Math
+a = int(input("enter the first cordiants on the x axis "))
+b  = int(input("enter the second cordiants on the y axis "))
+cordinates = (a, b)
+#the line below can be writen as distance = (a**2 + b**2)    
+distance = (cordinates[0]**2 + cordinates[1]**2)
+the_eucladian_distance = Math.sqrt(distance) 
+print(the_eucladian_distance)
+#this code till here  is  used to calculate between a point and the origin (0,0)
+# whtat if we wanted to calculate the distance of two points
+# we can use this formula
+#distance = Math.sqrt((x2 - x1)**2 + (y2 - y1 )**2)
+# we also have to define the second point and ask the user for it 
+#a2 = int(input("enter the second cordiants on the x axis "))
+#b2 = int(input("enter the second cordiants on the y axis "))
+# second_cordiantes = (a2 , b2)
