d9b080f6c3b20a5f58449aacfdcb96645e24f7a3
Amirmohammad <amirmohammadramezani1385@gmail.com>
2025-11-02 19:05:12 +0330

    some new things

--- Truth table/TruthTable.py
+++ Truth table/TruthTable.py
@@ -1,23 +1,40 @@
 from itertools import product
-
-
-
-
-
+import time
+from itertools import product
+from rich.live import Live
+from rich.console import Console
+from rich.table import Table
 def Truth_table(variables , formula):
-    print(" | " .join(variables) + " | Result" )# prints the lines between vars
-    print("-" *( 4 * len(variables) + 9) )# prints the line beneeth the vars and result 
+    table = Table(title="Truth Table")
+    for var in variables:
+        table.add_column(var)
+
+    table.add_column("result")
+
+    #print(" | " .join(variables) + " | Result" )# prints the lines between vars
+    #print("-" *( 4 * len(variables) + 9) )# prints the line beneeth the vars and result 
 # we have to have 2 arguments with this one . first one the number of state a variable can be in , in this case 2
 # secoond repeat means how many times it is going to make groups and try all the combinations 
-    for values in product([True , False] , repeat=len(variables)): 
+    for values in product([True , False] , repeat=len(variables)):
         eval_env = dict(zip(variables , values))
+        
         try:
             result = eval(formula , {} , eval_env )
+            
         except Exception as e:
                 print(f"err evaluting formula : {e}")
                 return
-        row = " | ".join(['1' if v else '0' for v in values]) + " | " + ('1' if result else '0') 
-        print(row)
+        
+        row_values = [ '1' if v else '0' for v in values ]
+        result_str = '1' if result else '0'
+        table.add_row(*row_values, result_str)
+
+        
+        #row = " | ".join(['1' if v else '0' for v in values]) + " | " + ('1' if result else '0') 
+        #print(row)
+    console = Console()
+    console.print(table)
+
     
 if __name__ == "__main__" : # for making this useable for anyone who wants to import , the name is equal to main when its directly executed 
      vars = input("enter varrs : ").replace(" ","").split(",")
